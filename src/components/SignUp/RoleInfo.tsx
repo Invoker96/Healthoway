172b98bdee3cd09f14c6ddbbc66fee6c33bdbd56
Yvonne Lee <eroz.undiscovered@gmail.com>
2022-10-23 23:09:56 -0400

    feat: Add step 1 - role selection

--- src/components/SignUp/RoleInfo.tsx
+++ src/components/SignUp/RoleInfo.tsx
@@ -1,5 +1,6 @@
 import { injectIntl } from 'react-intl';
-import { Typography } from '@mui/material';
+import { Button, Grid, IconButton, Typography } from '@mui/material';
+import { LocalHospital, Masks, PsychologyAlt } from '@mui/icons-material';
 
 type Props = {
   intl: any;
@@ -7,11 +8,38 @@ type Props = {
 
 const RoleInfo = ({ intl }: Props) => {
   return (
-    <Typography>
-      {intl.formatMessage({
-        id: 'role.label'
-      })}
-    </Typography>
+    <Grid>
+      <Typography>
+        {intl.formatMessage({
+          id: 'role.label'
+        })}
+      </Typography>
+      <Grid container>
+        <Grid item xs={4}>
+          <IconButton size="large">
+            <Masks />
+            <Typography>{intl.formatMessage({ id: 'role.button.patient' })}</Typography>
+          </IconButton>
+        </Grid>
+        <Grid item xs={4}>
+          <IconButton size="large">
+            <PsychologyAlt />
+            <Typography>{intl.formatMessage({ id: 'role.button.counsellor' })}</Typography>
+          </IconButton>
+        </Grid>
+        <Grid item xs={4}>
+          <IconButton size="large">
+            <LocalHospital />
+            <Typography>{intl.formatMessage({ id: 'role.button.doctor' })}</Typography>
+          </IconButton>
+        </Grid>
+      </Grid>
+      <Button variant="contained">
+        {intl.formatMessage({
+          id: 'role.button.next'
+        })}
+      </Button>
+    </Grid>
   );
 };
 
