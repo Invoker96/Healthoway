e50be61c045114f66d79c5833abcccea190d4b77
Merlin <merlinabraham95@gmail.com>
2022-10-27 14:58:16 -0400

    HW-46 Login page creation for the frontend

--- web/src/views/Login/Login.tsx
+++ web/src/views/Login/Login.tsx
@@ -1,12 +1,26 @@
 import { Link } from 'react-router-dom';
 
-import { Button } from '@mui/material';
+import { Button, Grid } from '@mui/material';
+import LoginForm from '../../components/Login/LoginForm';
+import { useMemo, useState } from 'react';
+import img from '../../assets/logo.png';
+import './Login.scss';
 
 const Login = () => {
+  const [formData, setFormData] = useState({
+    email: '',
+    password: ''
+  });
+
+  const loginPage = useMemo(() => {
+    return [<LoginForm formData={formData} setFormData={setFormData} />];
+  }, [formData]);
+
   return (
-    <Button component={Link} to={'/register'}>
-      Sign up
-    </Button>
+    <Grid container direction="column" className="sign-up-container">
+      <img src={img} className="logo" height="400" width="400" />
+      {loginPage}
+    </Grid>
   );
 };
 
