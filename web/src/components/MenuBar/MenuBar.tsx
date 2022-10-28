8025fedb898ef12a1a4dbcf712f67ed9591d2778
Siddhartha nanda <40684561+Invoker96@users.noreply.github.com>
2022-10-28 16:08:11 -0400

    Website landing page for the users (#9)
    Siddhartha <siddartha.nanda24@gmail.com>

---
+++ web/src/components/MenuBar/MenuBar.tsx
@@ -0,0 +1,40 @@
+import { injectIntl } from 'react-intl';
+import AppBar from '@mui/material/AppBar';
+import { Link } from 'react-router-dom';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Button from '@mui/material/Button';
+import Image from '../../assests/logo.png';
+import './MenuBar.scss';
+
+type Props = {
+  intl: any;
+};
+
+const MenuBar = ({ intl }: Props) => {
+  return (
+    <Box sx={{ display: 'flex' }}>
+      <AppBar component="nav">
+        <Toolbar>
+          <Box sx={{ flexGrow: 1, display: { xs: 'flex', sm: 'flex' } }}>
+            <img className="home_logo" src={Image}></img>
+          </Box>
+          <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
+            <Button className="actionBtn" variant="contained" component={Link} to={'/login'}>
+              {intl.formatMessage({
+                id: 'authForm.button.submit'
+              })}
+            </Button>
+            <Button className="actionBtn" variant="contained" component={Link} to={'/register'}>
+              {intl.formatMessage({
+                id: 'authForm.button.signup'
+              })}
+            </Button>
+          </Box>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+};
+
+export default injectIntl(MenuBar);
--- web/src/views/AppRoutes.tsx
+++ web/src/views/AppRoutes.tsx
@@ -4,13 +4,15 @@ import DoctorHome from './Doctor/DoctorHome';
 import Login from './Login/Login';
 import ManagerHome from './Manager/ManagerHome';
 import PatientHome from './Patient/PatientHome';
+import LandingPage from './LandingPage/LandingPage';
 import SignUp from './SignUp/SignUp';
 
 const AppRoutes = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path="/" element={<LandingPage />} />
+        <Route path="login" element={<Login />} />
         <Route path="register" element={<SignUp />} />
         <Route path="doctorHome" element={<DoctorHome />} />
         <Route path="patientHome" element={<PatientHome />} />
