be68bf650c6a3b06294540e904ac4b2af49d1980
Siddhartha <info@concordiaecsga.ca>
2022-10-29 13:12:18 -0400

    Updated landing page image and footer styling

--- web/src/views/LandingPage/LandingPage.tsx
+++ web/src/views/LandingPage/LandingPage.tsx
@@ -1,7 +1,7 @@
 import { injectIntl } from 'react-intl';
 import { Grid, Typography, Box } from '@mui/material';
 import MenuBar from '../../components/MenuBar/MenuBar';
-import bg from '../../assets/LandingImg.jpg';
+import bg from '../../assets/LandingImage.jpg';
 import './LandingPage.scss';
 
 type Props = {
@@ -16,56 +16,56 @@ const LandingPage = ({ intl }: Props) => {
       <Box sx={{ flexGrow: 1, display: { xs: 'flex', sm: 'flex' } }}></Box>
       <Grid container className="footer-container">
         <Grid item xs={4} sx={{ borderRight: 1 }}>
-          <Typography variant="h1" sx={{ borderBottom: 1, width: '70%', ml: '15%' }}>
+          <Typography variant="h1" sx={{ borderBottom: 1, width: '70%', ml: '15%', pt: 3 }}>
             {intl.formatMessage({
               id: 'landingPage.workingHoursTitle'
             })}
           </Typography>
-          <Typography variant="h5" sx={{ p: 2, color: '#9e9e9e' }}>
+          <Typography variant="h5" sx={{ p: 2, color: '#fff' }}>
             {intl.formatMessage({
               id: 'landingPage.workingHours1'
             })}
           </Typography>
-          <Typography variant="h5" sx={{ p: 2, color: '#9e9e9e' }}>
+          <Typography variant="h5" sx={{ p: 2, color: '#fff' }}>
             {intl.formatMessage({
               id: 'landingPage.workingHours2'
             })}
           </Typography>
-          <Typography variant="h5" sx={{ p: 2, color: '#9e9e9e' }}>
+          <Typography variant="h5" sx={{ p: 2, color: '#fff' }}>
             {intl.formatMessage({
               id: 'landingPage.workingHours3'
             })}
           </Typography>
         </Grid>
         <Grid item xs={4} sx={{ borderRight: 1 }}>
-          <Typography variant="h1" sx={{ borderBottom: 1, width: '70%', ml: '15%' }}>
+          <Typography variant="h1" sx={{ borderBottom: 1, width: '70%', ml: '15%', pt: 3 }}>
             {intl.formatMessage({
               id: 'landingPage.patientReviewTitle'
             })}
           </Typography>
-          <Typography variant="h5" sx={{ p: 2, color: '#9e9e9e', width: '70%', ml: '15%' }}>
+          <Typography variant="h5" sx={{ p: 2, color: '#fff', width: '70%', ml: '15%' }}>
             {intl.formatMessage({
               id: 'landingPage.patientReview'
             })}
           </Typography>
-          <Typography variant="h5" sx={{ p: 2, color: '#9e9e9e', width: '70%', ml: '15%' }}>
+          <Typography variant="h5" sx={{ p: 2, color: '#fff', width: '70%', ml: '15%' }}>
             {intl.formatMessage({
               id: 'landingPage.patientName'
             })}
           </Typography>
         </Grid>
         <Grid item xs={4}>
-          <Typography variant="h1" sx={{ borderBottom: 1, width: '70%', ml: '15%' }}>
+          <Typography variant="h1" sx={{ borderBottom: 1, width: '70%', ml: '15%', pt: 3 }}>
             {intl.formatMessage({
               id: 'landingPage.addressTitle'
             })}
           </Typography>
-          <Typography variant="h5" sx={{ p: 2, color: '#9e9e9e', width: '70%', ml: '15%' }}>
+          <Typography variant="h5" sx={{ p: 2, color: '#fff', width: '70%', ml: '15%' }}>
             {intl.formatMessage({
               id: 'landingPage.addressLine1'
             })}
           </Typography>
-          <Typography variant="h5" sx={{ p: 2, color: '#9e9e9e', width: '70%', ml: '15%' }}>
+          <Typography variant="h5" sx={{ p: 2, color: '#fff', width: '70%', ml: '15%' }}>
             {intl.formatMessage({
               id: 'landingPage.addressLine2'
             })}
