b4f215651c7afcb3ecde9e0f4a8563dcc691947c
Siddhartha <info@concordiaecsga.ca>
2022-11-02 16:00:17 -0400

    Added doctor and patient page

--- web/src/views/Doctor/DoctorHome.tsx
+++ web/src/views/Doctor/DoctorHome.tsx
@@ -1,7 +1,105 @@
 import { injectIntl } from 'react-intl';
+import { Button, Grid, Typography } from '@mui/material';
 
-const DoctorHome = () => {
-  return <div>DoctorHome</div>;
+type Props = {
+  intl: any;
+};
+
+const DoctorHome = ({ intl }: Props) => {
+  const PATIENTS_DATA = [
+    {
+      patient_name: 'Altouf',
+      assessment_form: 'text'
+    },
+    {
+      patient_name: 'Haider',
+      assessment_form: 'text'
+    },
+    {
+      patient_name: 'Dalie',
+      assessment_form: 'text'
+    },
+    {
+      patient_name: 'Ray',
+      assessment_form: 'text'
+    },
+    {
+      patient_name: 'Jack',
+      assessment_form: 'text'
+    },
+    {
+      patient_name: 'Oliver',
+      assessment_form: 'text'
+    }
+  ];
+
+  const tableRows = PATIENTS_DATA.map((info, index) => {
+    return (
+      <tr>
+        <td>{index + 1}</td>
+        <td>{info.patient_name}</td>
+        <td>
+          {' '}
+          <Button variant="contained">
+            {intl.formatMessage({
+              id: 'global.self_assessment_title'
+            })}
+          </Button>
+        </td>
+        <td>
+          {' '}
+          <Button variant="contained">
+            {intl.formatMessage({
+              id: 'global.reject'
+            })}
+          </Button>
+        </td>
+      </tr>
+    );
+  });
+
+  return (
+    <Grid sx={{ mt: 3 }}>
+      <Grid container justifyContent="center">
+        <Typography variant="h1">
+          {intl.formatMessage({
+            id: 'doctor.title'
+          })}
+        </Typography>
+      </Grid>
+      <Grid container sx={{ display: { xs: 'flex', sm: 'flex', justifyContent: 'center' } }}>
+        <Typography variant="h3" style={{ margin: '20px' }}>
+          {intl.formatMessage({
+            id: 'global.my_appointments'
+          })}
+        </Typography>
+        <Grid container justifyContent="space-around">
+          <table className="table table-stripped">
+            <thead>
+              <tr>
+                <th style={{ padding: '10px' }}>
+                  {' '}
+                  <Typography variant="h3">
+                    {intl.formatMessage({
+                      id: 'global.s_no'
+                    })}
+                  </Typography>
+                </th>
+                <th>
+                  <Typography variant="h3">
+                    {intl.formatMessage({
+                      id: 'global.patient_name_title'
+                    })}
+                  </Typography>
+                </th>
+              </tr>
+            </thead>
+            <tbody>{tableRows}</tbody>
+          </table>
+        </Grid>
+      </Grid>
+    </Grid>
+  );
 };
 
 export default injectIntl(DoctorHome);
--- web/src/views/Patient/PatientHome.tsx
+++ web/src/views/Patient/PatientHome.tsx
@@ -1,7 +1,42 @@
 import { injectIntl } from 'react-intl';
+import { Button, Grid, Typography } from '@mui/material';
 
-const PatientHome = () => {
-  return <div>PatientHome</div>;
+type Props = {
+  intl: any;
+};
+
+const PatientHome = ({ intl }: Props) => {
+  return (
+    <Grid sx={{ mt: 3 }}>
+      <Grid container justifyContent="center">
+        <Typography variant="h1">
+          {intl.formatMessage({
+            id: 'patient.title'
+          })}
+        </Typography>
+      </Grid>
+      <Grid container sx={{ display: { xs: 'flex', sm: 'flex' } }}>
+        <Button variant="contained" style={{ margin: '20px' }}>
+          {intl.formatMessage({
+            id: 'patient.self_assessment'
+          })}
+        </Button>
+      </Grid>
+      <Grid container>
+        <Typography
+          variant="h3"
+          sx={{ display: { xs: 'flex', sm: 'flex', justifyContent: 'center' } }}
+          style={{ margin: '20px' }}
+        >
+          <u>
+            {intl.formatMessage({
+              id: 'global.my_appointments'
+            })}
+          </u>
+        </Typography>
+      </Grid>
+    </Grid>
+  );
 };
 
 export default injectIntl(PatientHome);
