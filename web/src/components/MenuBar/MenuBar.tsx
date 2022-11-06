81a794ea8f5b26fa3d299d90a0fac15b7b274324
Siddhartha nanda <40684561+Invoker96@users.noreply.github.com>
2022-11-05 20:11:02 -0400

    Feat/hw 6 (#21)
    * Added counsellor dashboard page
    
    * Added doctor and patient page
    
    * Created footer component and major changes in custom dashboards
    
    Co-authored-by: Siddhartha

--- web/src/components/MenuBar/MenuBar.tsx
+++ web/src/components/MenuBar/MenuBar.tsx
@@ -6,12 +6,15 @@ import Toolbar from '@mui/material/Toolbar';
 import Button from '@mui/material/Button';
 import Image from '../../assets/logo.png';
 import './MenuBar.scss';
+import { Typography } from '@mui/material';
 
 type Props = {
   intl: any;
+  isCustomView: any;
+  title: any;
 };
 
-const MenuBar = ({ intl }: Props) => {
+const MenuBar = ({ intl, isCustomView, title }: Props) => {
   return (
     <Box sx={{ display: 'flex' }}>
       <AppBar component="nav">
@@ -19,29 +22,56 @@ const MenuBar = ({ intl }: Props) => {
           <Box sx={{ flexGrow: 1, display: { xs: 'flex', sm: 'flex' } }}>
             <img className="home_logo" src={Image}></img>
           </Box>
-          <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
-            <Button
-              className="actionBtn"
-              variant="outlined"
-              color="secondary"
-              component={Link}
-              to={'/login'}
-            >
+          <Box sx={{ flexGrow: 1, display: { xs: 'flex', sm: 'flex' } }}>
+            <Typography variant="h1">
               {intl.formatMessage({
-                id: 'authForm.button.submit'
+                id: title
               })}
-            </Button>
-            <Button
-              className="actionBtn"
-              variant="outlined"
-              color="secondary"
-              component={Link}
-              to={'/register'}
-            >
-              {intl.formatMessage({
-                id: 'authForm.button.signup'
-              })}
-            </Button>
+            </Typography>
+          </Box>
+
+          <Box
+            sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
+            style={{ position: 'absolute', right: '0', top: '15px' }}
+          >
+            {!isCustomView ? (
+              <>
+                <Button
+                  className="actionBtn"
+                  variant="outlined"
+                  color="secondary"
+                  component={Link}
+                  to={'/login'}
+                >
+                  {intl.formatMessage({
+                    id: 'authForm.button.submit'
+                  })}
+                </Button>
+                <Button
+                  className="actionBtn"
+                  variant="outlined"
+                  color="secondary"
+                  component={Link}
+                  to={'/register'}
+                >
+                  {intl.formatMessage({
+                    id: 'authForm.button.signup'
+                  })}
+                </Button>
+              </>
+            ) : (
+              <Button
+                className="actionBtn"
+                variant="outlined"
+                color="secondary"
+                component={Link}
+                to={'/'}
+              >
+                {intl.formatMessage({
+                  id: 'authForm.button.logout'
+                })}
+              </Button>
+            )}
           </Box>
         </Toolbar>
       </AppBar>
--- web/src/views/LandingPage/LandingPage.tsx
+++ web/src/views/LandingPage/LandingPage.tsx
@@ -3,6 +3,7 @@ import { Grid, Typography, Box } from '@mui/material';
 import MenuBar from '../../components/MenuBar/MenuBar';
 import bg from '../../assets/LandingImage.jpg';
 import './LandingPage.scss';
+import Footer from '../../components/Footer/Footer';
 
 type Props = {
   intl: any;
@@ -11,68 +12,15 @@ type Props = {
 const LandingPage = ({ intl }: Props) => {
   return (
     <>
-      <MenuBar />
+      <MenuBar
+        isCustomView={false}
+        title={intl.formatMessage({
+          id: 'global.empty'
+        })}
+      />
       <img className="bg-img" src={bg}></img>
       <Box sx={{ flexGrow: 1, display: { xs: 'flex', sm: 'flex' } }}></Box>
-      <Grid container className="footer-container">
-        <Grid item xs={4} sx={{ borderRight: 1 }}>
-          <Typography variant="h1" sx={{ borderBottom: 1, width: '70%', ml: '15%', pt: 3 }}>
-            {intl.formatMessage({
-              id: 'landingPage.workingHoursTitle'
-            })}
-          </Typography>
-          <Typography variant="h5" sx={{ p: 2, color: '#fff' }}>
-            {intl.formatMessage({
-              id: 'landingPage.workingHours1'
-            })}
-          </Typography>
-          <Typography variant="h5" sx={{ p: 2, color: '#fff' }}>
-            {intl.formatMessage({
-              id: 'landingPage.workingHours2'
-            })}
-          </Typography>
-          <Typography variant="h5" sx={{ p: 2, color: '#fff' }}>
-            {intl.formatMessage({
-              id: 'landingPage.workingHours3'
-            })}
-          </Typography>
-        </Grid>
-        <Grid item xs={4} sx={{ borderRight: 1 }}>
-          <Typography variant="h1" sx={{ borderBottom: 1, width: '70%', ml: '15%', pt: 3 }}>
-            {intl.formatMessage({
-              id: 'landingPage.patientReviewTitle'
-            })}
-          </Typography>
-          <Typography variant="h5" sx={{ p: 2, color: '#fff', width: '70%', ml: '15%' }}>
-            {intl.formatMessage({
-              id: 'landingPage.patientReview'
-            })}
-          </Typography>
-          <Typography variant="h5" sx={{ p: 2, color: '#fff', width: '70%', ml: '15%' }}>
-            {intl.formatMessage({
-              id: 'landingPage.patientName'
-            })}
-          </Typography>
-        </Grid>
-        <Grid item xs={4}>
-          <Typography variant="h1" sx={{ borderBottom: 1, width: '70%', ml: '15%', pt: 3 }}>
-            {intl.formatMessage({
-              id: 'landingPage.addressTitle'
-            })}
-          </Typography>
-          <Typography variant="h5" sx={{ p: 2, color: '#fff', width: '70%', ml: '15%' }}>
-            {intl.formatMessage({
-              id: 'landingPage.addressLine1'
-            })}
-          </Typography>
-          <Typography variant="h5" sx={{ p: 2, color: '#fff', width: '70%', ml: '15%' }}>
-            {intl.formatMessage({
-              id: 'landingPage.addressLine2'
-            })}
-          </Typography>
-        </Grid>
-      </Grid>
-      {/* </Container> */}
+      <Footer />
     </>
   );
 };
