9454127002ae16c5c6dee6b58aa52caae1189c73
Rajat Kumar <rajat1895@gmail.com>
2022-11-19 23:42:10 -0500

    Added custome dashboard and add user functionality by manager. (#52)

---
+++ web/src/components/ManagerRegister/RoleForm.tsx
@@ -0,0 +1,92 @@
+import { useEffect, useState } from 'react';
+import { injectIntl } from 'react-intl';
+import { Button, Grid, Typography } from '@mui/material';
+import { LocalHospital, Masks, PsychologyAlt } from '@mui/icons-material';
+import { User, RoleType } from '../../types';
+import './SignUp.scss';
+import './RoleForm.scss';
+
+type Props = {
+  intl: any;
+  page: number;
+  setPage: React.Dispatch<number>;
+  formData: User;
+  setFormData: React.Dispatch<User>;
+};
+
+const RoleForm = ({ page, setPage, formData, setFormData, intl }: Props) => {
+  const [userRole, setUserRole] = useState(
+    formData['userRole'] ? formData['userRole'] : RoleType.PATIENT
+  );
+
+  useEffect(() => {
+    setFormData({
+      //reset form data
+      ...{
+        fullName: '',
+        email: '',
+        password: '',
+        userRole: RoleType.PATIENT,
+        dob: new Date().toISOString().split('T')[0],
+        pNum: ''
+      },
+      userRole: userRole
+    });
+  }, [setFormData, userRole]);
+
+  return (
+    <Grid className="form-card">
+      <Typography variant="h1">
+        {intl.formatMessage({
+          id: 'manager.alternateTitle'
+        })}
+      </Typography>
+      <Grid container justifyContent="space-around">
+        <Grid
+          item
+          xs={3}
+          className={`role-card-container ${userRole === RoleType.PATIENT ? 'selected' : ''}`}
+        >
+          <Button size="large" onClick={() => setUserRole(RoleType.PATIENT)}>
+            <Masks />
+            <Typography>{intl.formatMessage({ id: 'role.patient' })}</Typography>
+          </Button>
+        </Grid>
+        <Grid
+          item
+          xs={3}
+          className={`role-card-container ${userRole === RoleType.COUNSELLOR ? 'selected' : ''}`}
+        >
+          <Button size="large" onClick={() => setUserRole(RoleType.COUNSELLOR)}>
+            <PsychologyAlt />
+            <Typography>{intl.formatMessage({ id: 'role.counsellor' })}</Typography>
+          </Button>
+        </Grid>
+        <Grid
+          item
+          xs={3}
+          className={`role-card-container ${userRole === RoleType.DOCTOR ? 'selected' : ''}`}
+        >
+          <Button size="large" onClick={() => setUserRole(RoleType.DOCTOR)}>
+            <LocalHospital />
+            <Typography>{intl.formatMessage({ id: 'role.doctor' })}</Typography>
+          </Button>
+        </Grid>
+      </Grid>
+      <Grid container className="button-container">
+        <Button
+          variant="contained"
+          onClick={() => {
+            setPage(page + 1);
+          }}
+        >
+          {intl.formatMessage({
+            id: 'userForm.button.next'
+          })}
+        </Button>
+      </Grid>
+    </Grid>
+  );
+};
+
+export default injectIntl(RoleForm);
--- web/src/views/Manager/ManagerHome.tsx
+++ web/src/views/Manager/ManagerHome.tsx
@@ -1,7 +1,106 @@
 import { injectIntl } from 'react-intl';
+import { Button, Card, Grid, Typography } from '@mui/material';
+import MenuBar from '../../components/MenuBar/MenuBar';
+import './ManagerHome.scss';
+import FooterComp from '../../components/FooterComp/FooterComp';
+import { useNavigate, useLocation, Link } from 'react-router-dom';
+import AppSnackbar from '../../components/AppSnackbar/AppSnackbar';
+import { getFullName } from '../../services/userInfoService';
 
-const ManagerHome = () => {
-  return <div>ManagerHome</div>;
+type Props = {
+  intl: any;
+};
+
+const ManagerHome = ({ intl }: Props) => {
+  //clear state on refresh so snack bar is not shown
+  window.history.replaceState({}, document.title);
+
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const successMessage = location.state?.successMessage;
+
+  const navigateToManagerRegister = () => {
+    navigate('/manager/home/add');
+  };
+
+  return (
+    <>
+      {successMessage && <AppSnackbar type="success" message={successMessage} open={true} />}
+      <MenuBar
+        isLoggedIn={true}
+        title={intl.formatMessage({
+          id: 'manager.title'
+        })}
+        noBtn={false}
+      />
+      <Grid container className="main-container">
+        <Grid container justifyContent="center">
+          <Card className="doctorHome-greeting-container">
+            <Typography variant="h1" style={{ margin: '20px' }}>
+              {intl.formatMessage(
+                {
+                  id: 'manager.label.greetings'
+                },
+                {
+                  fullName: getFullName()
+                }
+              )}
+            </Typography>
+            <Typography variant="h3" style={{ margin: '20px' }}>
+              {intl.formatMessage({
+                id: 'manager.label.desc1'
+              })}
+            </Typography>
+            <Typography variant="h3" style={{ margin: '20px' }}>
+              {intl.formatMessage({
+                id: 'manager.label.desc2'
+              })}
+            </Typography>
+          </Card>
+        </Grid>
+      </Grid>
+      <Grid container className="margin-left">
+        <Button
+          onClick={navigateToManagerRegister}
+          variant="contained"
+          style={{ margin: '20px' }}
+          sx={{ display: { xs: 'flex', sm: 'flex', width: 'fit-content' } }}
+        >
+          {intl.formatMessage({
+            id: 'manager.add'
+          })}
+        </Button>
+      </Grid>
+      <Grid container className="margin-left">
+        <Button
+          variant="contained"
+          style={{ margin: '20px' }}
+          sx={{ display: { xs: 'flex', sm: 'flex', width: 'fit-content' } }}
+          component={Link}
+          to={'/'}
+        >
+          {intl.formatMessage({
+            id: 'manager.remove'
+          })}
+        </Button>
+      </Grid>
+      <Grid container className="margin-left">
+        <Button
+          variant="contained"
+          style={{ margin: '20px' }}
+          sx={{ display: { xs: 'flex', sm: 'flex', width: 'fit-content' } }}
+          component={Link}
+          to={'/'}
+        >
+          {intl.formatMessage({
+            id: 'manager.users'
+          })}
+        </Button>
+      </Grid>
+      <FooterComp />
+    </>
+  );
 };
 
 export default injectIntl(ManagerHome);
